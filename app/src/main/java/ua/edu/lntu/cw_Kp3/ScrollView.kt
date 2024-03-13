aaa468d965aeaef9c9abc2a841f3ccac98d75c1a
Kindzhi <radishevsky.sergiy@gmail.com>
2024-03-13 16:20:17 +0200

    Initial commit

---
+++ app/src/main/java/ua/edu/lntu/cw_Kp3/ScrollView.kt
@@ -0,0 +1,11 @@
+package ua.edu.lntu.cw_Kp3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll_view)
+    }
+}
